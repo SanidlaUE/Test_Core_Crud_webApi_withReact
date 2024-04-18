25f35971688b664fb6a39486d20a2b2d2af9c56e
SanidlaUE <sanidla111@gmail.com>
2024-04-19 00:11:52 +0300

    Add project files.

---
+++ Test_Core_Crud_webApi_withReact/Contracts/BooksRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Test_Core_Crud_webApi_withReact.Contracts
+{
+    public record BooksRequest(       
+       string Title,
+       string Description,
+       decimal Price);
+}
---
+++ Test_Core_Crud_webApi_withReact/Contracts/BooksResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Test_Core_Crud_webApi_withReact.Contracts
+{
+    public record BooksResponse(
+        Guid Id,
+        string Title,
+        string Description,
+        decimal Price);
+}
